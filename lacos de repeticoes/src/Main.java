c512c459b2fbefc9953c9bb9ecd8ff446f15e1fa
Luiz Henrique <luiz25henrique@outlook.com>
2025-12-18 20:25:19 -0300

    Apaguei o código antigo e estou treinando o novo

--- lacos de repeticoes/src/Main.java
+++ lacos de repeticoes/src/Main.java
@@ -6,22 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite '1' para iniciar o seu dia");
+        System.out.println("Informe quando a sua encomenda chegar");
 
-        int inicial = scanner.nextInt();
-
-        if (inicial == 1) {
-            LocalTime horaInicial = LocalTime.now();
-            LocalDate incioDaJornada = LocalDate.now();
-            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            DateTimeFormatter formartoDaHora = DateTimeFormatter.ofPattern("HH:mm");
-            System.out.println("A hora que você iniciouo seu dia " + formartoDaHora.format(horaInicial));
-            System.out.println("A data que você ínicio " + formato.format(incioDaJornada));
-        }
+        String dataChegada = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
 
-
-
+        System.out.println(dataChegada);
     }
-
 }
