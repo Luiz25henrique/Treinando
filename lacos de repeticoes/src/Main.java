60b50125cac095d340a6bcf1cbe3e02b321f06c5
Luiz Henrique <luiz25henrique@outlook.com>
2025-12-18 20:35:35 -0300

    Implementei a hora

--- lacos de repeticoes/src/Main.java
+++ lacos de repeticoes/src/Main.java
@@ -11,7 +11,9 @@ public class Main {
 
         if (scanner.nextInt() == 1) {
             String dataChegada = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            String horaDaCheada = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
             System.out.println("Sua mercaoria foi registrada a chegada dia: " + dataChegada);
+            System.out.println("A hora que sua mercadoria chegou " + horaDaCheada);
         } else {
             System.out.println("informe sua reclamação");
         }
