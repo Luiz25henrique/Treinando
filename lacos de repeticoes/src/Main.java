35c218ccdbbd6efa8d76dbf4f3bbd0242100ef05
Luiz Henrique <luiz25henrique@outlook.com>
2025-12-18 20:32:01 -0300

    Coloquei if e else no código

--- lacos de repeticoes/src/Main.java
+++ lacos de repeticoes/src/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Informe quando a sua encomenda chegar");
-
-        String dataChegada = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        System.out.println("Digite '1' para informar que sua entrega chegou");
 
 
-        System.out.println(dataChegada);
+        if (scanner.nextInt() == 1) {
+            String dataChegada = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            System.out.println("Sua mercaoria foi registrada a chegada dia: " + dataChegada);
+        } else {
+            System.out.println("informe sua reclamação");
+        }
+
     }
 }
